bed78ea23763157e9c45274992c3954206d84215
James Stuckey <jstuckey@rutgers.edu>
2019-08-01 16:58:47 -0400

    Configure mongo db

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://host.docker.internal:27017/journal'
 };
---
+++ db/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const logger = require('../modules/Logger');
+mongoose.Promise = global.Promise;
+
+const { MONGODB_URI } = require('../config');
+
+function dbConnect(url = MONGODB_URI) {
+    return mongoose.connect(url, { useNewUrlParser: true })
+    .catch(err => {
+        logger.error('Mongoose failed to connect');
+        logger.error(err);
+    });
+}
+
+function dbDisconnect() {
+    return mongoose.disconnect();
+}
+
+function dbGet() {
+    return mongoose;
+}
+
+module.exports = {
+    dbConnect,
+    dbDisconnect,
+    dbGet,
+};
