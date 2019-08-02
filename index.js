c5ef58b6ac8e29aa968eced1416cac4bef7df5f0
James Stuckey <jstuckey@rutgers.edu>
2019-08-02 18:22:54 -0400

    Add error handler

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+    'use strict';
 
 const express = require('express');
 const cors = require('cors');
@@ -8,6 +8,9 @@ const morgan = require('morgan');
 const passport = require('passport');
 const localStrategy = require('./passport/local');
 const jwtStrategy = require('./passport/jwt');
+
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 
@@ -35,10 +38,29 @@ app.use(express.json());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    logger.error(err);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (err.status) {
+        const errBody = Object.assign({}, err, { message: err.message });
+        logger.error(err.message);
+        res.status(err.status).json(errBody);
+    }
+    if (err.message) {
+        res.json({ err: err.message });
+    } else {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 
 function runServer(port = PORT) {
     const server = app
